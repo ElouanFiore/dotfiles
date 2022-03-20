a26d55494b9ce1f85249fc3f665c6248cf699743
ElouanFiore <96867225+ElouanFiore@users.noreply.github.com>
2022-03-20 19:43:15 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
 mkinstall() {
-	#dd if=$3 of=$1
+	#eval "dd if=$3 of=$1 bs=4k" 
+	
+	declare -i i=1
+	file=()
+	echo "Select device:\n"
+	for $f in $1*; do
+		echo "$i) $f"
+		i=$i+1
+		file+=$f
+	done
+	echo "\n"
+	read 
+	
 	dev=$(eval "ls $1*" | fzf)
 	eval "cp -v ~/.dotfiles/install.sh $dev"
 	eval "cp -v ~/.ssh/github_key $dev"
