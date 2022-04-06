83b0f051dc15dd6f3405d0f9d83f640db5ad0142
ElouanFiore <96867225+ElouanFiore@users.noreply.github.com>
2022-04-06 08:49:01 +0200

    Adaptation for red laptop

--- install.sh
+++ install.sh
@@ -1,31 +1,38 @@
 #!/bin/bash
 
 mkinstall() {
-	# Writing ISO $3 in device $1
-	eval "dd if=$3 of=$1 bs=4k" 
+	# Unmount all device's partions
+	for p in $device?; do
+		eval "sudo umount $p"
+	done
+
+	# Writing ISO in device
+	eval "sudo dd if=$iso of=$device bs=2M status=progress" 
 	
-	# Print device's partitions 
-	for p in /dev/sda?; do 
-			((c+=1))
-			parts+=($p)
-			echo "$c) $p"
+	# Print device's partitions
+	eval "sudo fdisk -l $device"	
+	for p in $device?; do 
+		parts+=($p)
 	done
 	read -p "Select partition : "
 	((c=$REPLY-1)); part=${parts[$c]}
 	
 	#Copy key and install script for second part
-	eval "cp -v ~/.dotfiles/install.sh $part"
-	eval "cp -v ~/.ssh/git_key $part"
+	eval "mkdir /tmp/ins && sudo mount $part /tmp/ins"
+	eval "cp -v ~/.dotfiles/install.sh /tmp/ins"
+	eval "cp -v ~/.ssh/git_key /tmp/ins"
 	
+	# Clearing
+	eval "umount $part"	
 	unset c p part parts
 }
 
 if [ $# -eq 3 ]; then
 	if [ $2 == "-i" ]; then
+		device=$1
+		iso=$3
 		mkinstall
 	fi
 elif [ $# -eq 1 ]; then
-	#install
-else
-	echo "Bad usage"	
+	echo "test"
 fi
